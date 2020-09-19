9f1232f285d2d64f30fb9931c9b58f53d351633a
thierry <ngo.consulting.ngo@gmail.com>
2020-09-19 23:20:59 +0200

    commit-V0.19.06.20

---
+++ Users/L512/Documents/spring boot/inscription/serverGestionProduits/src/main/java/com/ngo/serverGestionProduits/ServerGestionProduitsApplication.java
@@ -0,0 +1,13 @@
+package com.ngo.serverGestionProduits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGestionProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGestionProduitsApplication.class, args);
+	}
+
+}
